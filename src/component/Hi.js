294660bf569b9e4ac6ff94b478ddcaedecec9209
memosize <89068024@qq.com>
2019-07-21 12:26:31 +0800

    first ts componnet

---
+++ src/component/Hi.js
@@ -0,0 +1,12 @@
+import React, {Componnet} from 'react'
+import {View, Text} from 'react-native'
+
+export default class Hi extends React.Component {
+   render(){
+       return(
+           <View>
+               <Text>{'sdfsd'}</Text>
+           </View>
+       )
+   }
+}
