ef787f230843b7e82e93c6169550cccfb7ebd66e
memosize <89068024@qq.com>
2019-07-21 12:31:15 +0800

    delete unused code

--- App.tsx
+++ App.tsx
@@ -1,34 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from "react";
-import Fragment from "react";
-import { Component } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar
-} from "react-native";
- import Hello from './src/component/Hello'
-import Hi from './src/component/Hi'
+import { View } from "react-native";
+import Hello from "./src/component/Hello";
 
 const App = () => {
   return (
     <View>
-      <Hi/>
-      <Hello name={'ss'} enthusiasmLevel={88}/>
+      <Hello name={"ss"} enthusiasmLevel={88} />
     </View>
   );
 };
-
-
 
 export default App;
